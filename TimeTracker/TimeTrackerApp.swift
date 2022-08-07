2a8518546a4ddd3323083e9fa224f0bdd26b939c
Mathilde Ferrand <mathilde.ferrand.coeurderoy@gmail.com>
2022-08-07 12:18:46 +0100

    chore: initial commit

---
+++ TimeTracker/TimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerApp.swift
+//  TimeTracker
+//
+//  Created by Mathilde Ferrand on 07/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

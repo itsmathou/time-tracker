2562fbc0192f794f9ef2a93b6c9c0aac2d5e3188
Mathilde Ferrand <mathilde.ferrand.coeurderoy@gmail.com>
2022-08-07 15:40:04 +0100

    feat: removed some duplication to get the scheduleURL

--- TimeTracker/Scheduling/SchedulingViewModel.swift
+++ TimeTracker/Scheduling/SchedulingViewModel.swift
@@ -14,38 +14,34 @@ protocol Scheduling {
 final class SchedulingViewModel: Scheduling {
     
     func save(schedule: SchedulingModel.Schedule) {
-        let documentPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
-        let documentsUrl = NSURL(fileURLWithPath: documentPath)
-        let scheduleUrl = documentsUrl.appendingPathComponent("time-tracker.json")
-        
         if let existingSchedules = loadExistingSchedulesIfNeeded(), let scheduleUrl {
             var newListOfSchedules = existingSchedules.schedules
             newListOfSchedules.append(schedule)
             let newModel = SchedulingModel(schedules: newListOfSchedules)
             let data = try? JSONEncoder().encode(newModel)
             try? data?.write(to: scheduleUrl)
-            print(scheduleUrl)
         } else if let scheduleUrl {
             let model = SchedulingModel(schedules: [schedule])
             let data = try? JSONEncoder().encode(model)
             try? data?.write(to: scheduleUrl)
-            print(scheduleUrl)
         } else {
-            print("I did something really wrong??")
+            fatalError("Something went really wrong")
         }
-        
-        
-        print("schedule has been saved! \(schedule)")
     }
 }
 
 private extension SchedulingViewModel {
+    var scheduleUrl: URL? {
+        let documentPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let documentsUrl = NSURL(fileURLWithPath: documentPath)
+        return documentsUrl.appendingPathComponent("time-tracker.json")
+    }
+
     func loadExistingSchedulesIfNeeded() -> SchedulingModel? {
         let documentPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
         let documentsUrl = NSURL(fileURLWithPath: documentPath)
-        guard let scheduleUrl = documentsUrl.appendingPathComponent("time-tracker.json"),
+        guard let scheduleUrl,
               let existingSchedules = try? Data(contentsOf: scheduleUrl) else {
-            print("No schedule found")
             return nil
         }
         return try? JSONDecoder().decode(SchedulingModel.self, from: existingSchedules)

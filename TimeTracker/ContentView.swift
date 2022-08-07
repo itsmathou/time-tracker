4d152583065f9efd481feae0db4849ab54a94b39
Mathilde Ferrand <38668010+itsmathou@users.noreply.github.com>
2022-08-07 19:13:55 +0100

    feat: made sidebar with default selections, colours and changed order (#9)

--- TimeTracker/ContentView.swift
+++ TimeTracker/ContentView.swift
@@ -8,27 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var item: Item? = .createSchedule
+
     var body: some View {
         NavigationView {
             List {
-                NavigationLink {
-                    SchedulingView(viewModel: SchedulingViewModel())
-                } label: {
-                    Image(systemName: "calendar.badge.clock")
+                Section("sidebar_section_first_title") {                    
+                    NavigationLink(tag: Item.createSchedule, selection: $item) {
+                        SchedulesView(viewModel: SchedulesViewModel())
+                    } label: {
+                        sidebarLabel("schedules_title", iconName: "calendar.badge.clock")
+                    }
+
                     
-                    Text("scheduling_title")
+                    NavigationLink(tag: Item.mySchedules, selection: $item) {
+                        SchedulingView(viewModel: SchedulingViewModel())
+                    } label: {
+                        sidebarLabel("scheduling_title", iconName: "calendar.badge.plus")
+                    }
                 }
-                
-                NavigationLink {
-                    SchedulesView(viewModel: SchedulesViewModel())
-                } label: {
-                    Image(systemName: "calendar")
-                    
-                    Text("schedules_title")
-                }
+                .headerProminence(.increased)
             }
         }
         .navigationTitle("app_title")
+    }
+}
+
+private extension ContentView {
+    func sidebarLabel(_ title: String.LocalizationValue, iconName: String) -> some View {
+        HStack {
+            Image(systemName: iconName)
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(.white, .red)
+            
+            Text(String(localized: title))
+        }
+        .padding(.leading, 5)
+    }
+    
+    enum Item: Hashable, Identifiable {
+        case createSchedule
+        case mySchedules
+        
+        var id: Item { self }
     }
 }
 

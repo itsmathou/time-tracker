f05b217700b0495c0d4a027204a59c8292c10e12
Mathilde Ferrand <mathilde.ferrand.coeurderoy@gmail.com>
2022-08-12 20:18:02 +0100

    chore: removed stubs and mockfilemanager from view

---
+++ TimeTracker/Schedules/Schedule+Stub.swift
@@ -0,0 +1,26 @@
+//
+//  Schedule+Stub.swift
+//  TimeTracker
+//
+//  Created by Mathilde Ferrand on 12/08/2022.
+//
+
+import Foundation
+
+#if DEBUG
+extension Schedule {
+    static let firstStubSchedule: Self = .init(
+        id: UUID(),
+        scheduleName: "July 2021",
+        startDate: Date.create(day: 1, month: 7, year: 2021)!,
+        endDate: Date.create(day: 31, month: 7, year: 2021)!
+    )
+    
+    static let secondStubSchedule: Self = .init(
+        id: UUID(),
+        scheduleName: "August 2021",
+        startDate: Date.create(day: 1, month: 8, year: 2021)!,
+        endDate: Date.create(day: 31, month: 8, year: 2021)!
+    )
+}
+#endif
